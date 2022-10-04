0f8b911000f2801fd2c60648bccacdf25f82058d
babbu47 <111335500+babbu47@users.noreply.github.com>
2022-10-05 02:03:56 +0530

    updated text

--- ap.js
+++ ap.js
@@ -1,3 +1,26 @@
-var input=document.querySelector("#input");
-var output=document.querySelector("#output");
-var url=""
+var inputtxt=document.querySelector("#input");
+var outputtxt=document.querySelector("#output");
+var trigger=document.querySelector("#bot");
+var url="https://api.funtranslations.com/translate/braille.json";
+function constructurl(tt)
+{
+    return url+"?"+"text="+tt
+}
+function error()
+{
+    return alert("Server is not working")
+}
+function triggerpoint()
+{
+    var inputvalue=inputtxt.value;
+    fetch(constructurl(inputvalue))
+    .then(response=>response.json)
+    .then(json=>
+        {
+            var outputt=json.contents.translated
+            outputtxt.innerText=outputt
+        })
+        .catch(error)
+        
+}
+trigger.addEventListener("click",triggerpoint)
