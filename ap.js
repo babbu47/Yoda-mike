7542800d96e58e5c87617388fa2a24757f8fcce8
babbu47 <111335500+babbu47@users.noreply.github.com>
2022-10-05 02:12:07 +0530

    language changed

--- ap.js
+++ ap.js
@@ -1,7 +1,7 @@
 var inputtxt=document.querySelector("#input");
 var outputtxt=document.querySelector("#output");
 var trigger=document.querySelector("#bot");
-var url="https://api.funtranslations.com/translate/braille.json";
+var url="https://api.funtranslations.com/translate/orcish.json";
 function constructurl(tt)
 {
     return url+"?"+"text="+tt
@@ -14,13 +14,13 @@ function triggerpoint()
 {
     var inputvalue=inputtxt.value;
     fetch(constructurl(inputvalue))
-    .then(response=>response.json)
+    .then(response=>response.json())
     .then(json=>
         {
-            var outputt=json.contents.translated
-            outputtxt.innerText=outputt
+            var outputt=json.contents.translated;
+            outputtxt.innerText=outputt;
         })
         .catch(error)
         
-}
+};
 trigger.addEventListener("click",triggerpoint)
